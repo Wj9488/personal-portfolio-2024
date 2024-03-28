683879fa1239e2ffcf7f4e63109f7dc1c3a75b55
Will Jones <wtjones10@gmail.com>
2024-03-28 11:55:15 +0000

    updated projects page

--- Components/ProjectsPage.jsx
+++ Components/ProjectsPage.jsx
@@ -18,48 +18,53 @@ const ProjectsPage = () => {
   return (
     <main>
         <section className="margin__setter mt-[5rem]">
-            <div className="ml-[1.25rem] lg:ml-[7.5rem] mb-[5rem] lg:mb-[10rem]">
+            <div className="ml-[1.25rem] lg:ml-[7.5rem] mb-[5rem] lg:mb-[10rem] flex">
                 <LargeHeader customLetter="W" headerText="ork" />
+                <Reveal delay={0.25}>
+                  <p className="italic ml-4 px-1">{"(x03)"}</p>
+                </Reveal>
             </div>
         </section>
         <section className="margin__setter mt-[4.5rem]" id="projects">
         <div className="relative">
-          <div className="mt-[1.25rem]">
+            <div className="mt-[1.25rem]">
+              <ProjectShowcase
+                projectTitle="Strong Words"
+                projectDetails="Created in collaboration with Array® for a content creation business focussing on the book review niche. The client tasked us with enhancing their brand’s online presence with a modern website rebrand. The business, wanting to cater to the growing demand for digital offerings, also tasked us with creating a sleek new email newsletter offering for readers.  "
+                projectLink="https://strong-words.co.uk"
+                projectLinkText="See live site"
+                imgAlt="Strong Words"
+                imgSource={StrongWordsImage}
+                thumbAlt="Strong Words Thumbnail"
+                thumbSource={StrongWordsThumb}
+                translationheight="-900px"
+              />
+            </div>
+            <div className="mt-[1.25rem]">
             <ProjectShowcase
-              projectTitle="Strong Words"
-              projectDetails="Created in collaboration with Array® for a content creation business focussing on the book review niche. The client tasked us with enhancing their brand’s online presence with a modern website rebrand. The business, wanting to cater to the growing demand for digital offerings, also tasked us with creating a sleek new email newsletter offering for readers.  "
-              projectLink="https://strong-words.co.uk"
-              projectLinkText="See live site"
-              imgAlt="Strong Words"
-              imgSource={StrongWordsImage}
-              thumbAlt="Strong Words Thumbnail"
-              thumbSource={StrongWordsThumb}
-              translationheight="-900px"
-            />
-          </div>
-          <ProjectShowcase
-              projectTitle="French For All"
-              projectDetails="A freelance project built for a professional native French Tutor doing both online and in-person teaching to clients. I was tasked with building a fully responsive, effective and modern website to convert more traffic from Facebook into paying clients."
-              projectLink="https://french-lesson.com"
-              projectLinkText="See live site"
-              imgAlt="French For All"
-              imgSource={FrenchForAllImage}
-              thumbAlt="French For All Thumb"
-              thumbSource={FrenchForAllThumb}
-              translationheight="-600px"
-            />
-          </div>
-          <div className="mt-[1.25rem] my-[5rem]">
-            <ProjectShowcase
-              projectTitle="Birdlime Media"
-              projectDetails="An ongoing project worked on during my internship at Thousand Lines® for a London based production company. Birdlime media makes videos and podcasts for charities broadcasters and brands focussing on developing new talent from underrepresented backgrounds and offering them paid work experience.  "
-              projectLink="#"
-              projectLinkText="Coming Soon"
-              imgAlt="Birdlime Media"
-              imgSource={BirdlimeImage}
-              translationheight="-600px"
-            />
-          </div>
+                projectTitle="French For All"
+                projectDetails="A freelance project built for a professional native French Tutor doing both online and in-person teaching to clients. I was tasked with building a fully responsive, effective and modern website to convert more traffic from Facebook into paying clients."
+                projectLink="https://french-lesson.com"
+                projectLinkText="See live site"
+                imgAlt="French For All"
+                imgSource={FrenchForAllImage}
+                thumbAlt="French For All Thumb"
+                thumbSource={FrenchForAllThumb}
+                translationheight="-600px"
+              />
+            </div>
+            <div className="mt-[1.25rem] my-[5rem]">
+              <ProjectShowcase
+                projectTitle="Birdlime Media"
+                projectDetails="An ongoing project worked on during my internship at Thousand Lines® for a London based production company. Birdlime media makes videos and podcasts for charities broadcasters and brands focussing on developing new talent from underrepresented backgrounds and offering them paid work experience.  "
+                projectLink="#"
+                projectLinkText="Coming Soon"
+                imgAlt="Birdlime Media"
+                imgSource={BirdlimeImage}
+                translationheight="-600px"
+              />
+            </div>
+        </div>
         </section>
         <FooterCta />
     </main>
